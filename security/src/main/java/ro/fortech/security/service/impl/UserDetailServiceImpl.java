20775a3ceae1fb78079e31747d7e37cee2406973
Vasile <gaciuvasy@yahoo.com>
2023-06-07 10:45:07 +0300

    Optimize imports

--- security/src/main/java/ro/fortech/security/service/impl/UserDetailServiceImpl.java
+++ security/src/main/java/ro/fortech/security/service/impl/UserDetailServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ro.fortech.security.entity.Users;
 import ro.fortech.security.repository.UsersRepository;
